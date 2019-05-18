672d5d50b5dcc2ef7689f3fe6c413f2cb8227b9b
CoyoteWaltz <coyote_waltz@163.com>
2019-05-18 18:33:54 +0800

    modify production_settings

---
+++ config/production_settings.py
@@ -0,0 +1,5 @@
+DEBUG = False
+SQLALCHEMY_ECHO = False          # 将sql语句都打印出来？
+SQLALCHEMY_DATABASE_URI = "mysql://root:ljw9898'@localhost/major_db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = 'utf-8'   #数据库编码
